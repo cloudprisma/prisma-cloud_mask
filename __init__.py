c8b89ee6721cd25478e93b0cd6f95168e7e2dd8b
cloudprisma <prisma.cloud.iac@gmail.com>
2024-09-25 11:49:09 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .src import preprocessing
+from .src import knn_classifier
+from .src import rf_classifier
+from .src import xgboost_classifier
+
